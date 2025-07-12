c92b0fca530747e5e04303da972b6772cf8297fc
sumitarora1990 <sumitarora.ea@gmail.com>
2025-07-12 06:04:24 +0000

    dataypes

---
+++ basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountID = 144553
+let accountEmail = "sumit@google.com"
+var accountPassword = "12345"
+accountCity = "Kanpur"
+let accountState;
+
+// accountID = 2
+console.log(accountID);
+
+accountEmail ="sa@sa.com"
+accountPassword = "2121212"
+accountCity="chandigarh"
+
+console.table([accountID,accountEmail,accountPassword,accountCity,accountState])
